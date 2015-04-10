74f57a4489242aa6694329169c375e089349bba3
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-04-10 09:15:10 +0200

    initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+
+
+    /* A */
+
+    /* B */
+
+    /* C */
+
+    /* D */
+
+    /* E */
+
+    /* F */
+
+    /* G */
+
+    /* H */
+
+    /* I */
+
+    /* J */
+
+    /* K */
+
+    /* L */
+
+    /* M */
+
+    /* N */
+
+    /* O */
+
+    /* P */
+
+    /* Q */
+
+    /* R */
+
+    /* S */
+
+    /* T */
+
+    /* U */
+
+    /* V */
+
+    /* W */
+
+    /* X */
+
+    /* Y */
+
+    /* Z */
+
+
+}
