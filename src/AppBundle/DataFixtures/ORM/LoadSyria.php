ce8358c689e5d724f3a2e49af2cc56d69744a8d8
LukaszB012 <LukaszB012@gmail.com>
2015-04-10 10:10:09 +0200

    Syria: kontroler i widok

--- src/AppBundle/DataFixtures/ORM/LoadSyria.php
+++ src/AppBundle/DataFixtures/ORM/LoadSyria.php
@@ -7,9 +7,9 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 use Symfony\Component\Yaml\Yaml;
 
-use AppBundle\Entity\litwa;
+use AppBundle\Entity\Syria;
 
-class Loadlitwa implements FixtureInterface
+class LoadSyria implements FixtureInterface
 {
     /**
      * {@inheritDoc}
@@ -17,13 +17,13 @@ class Loadlitwa implements FixtureInterface
     public function load(ObjectManager $manager)
     {
 
-        $filename = __DIR__ . '/../../../../data/litwa.yml';
+        $filename = __DIR__ . '/../../../../data/syria.yml';
         $yml = Yaml::parse(file_get_contents($filename));
         foreach ($yml as $item) {
-            $litwa = new litwa();
-            $litwa->setName($item['name']);
-            $litwa->setpopulation($item['population']);
-            $manager->persist($litwa);
+            $syria = new Syria();
+            $syria->setName($item['name']);
+            $syria->setDepth($item['depth']);
+            $manager->persist($syria);
         }
 
         $manager->flush();
