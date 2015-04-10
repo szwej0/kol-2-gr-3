2de9f346d7e42d24fe96a28ee32f727462f7b50c
mmajewski1313 <mmajewski1313@gmail.com>
2015-04-10 10:10:23 +0200

    Italy: kontroler i widok

--- src/AppBundle/DataFixtures/ORM/LoadItaly.php
+++ src/AppBundle/DataFixtures/ORM/LoadItaly.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace AppBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
@@ -7,9 +6,9 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 use Symfony\Component\Yaml\Yaml;
 
-use AppBundle\Entity\litwa;
+use AppBundle\Entity\Italy;
 
-class Loadlitwa implements FixtureInterface
+class LoadItaly implements FixtureInterface
 {
     /**
      * {@inheritDoc}
@@ -17,13 +16,13 @@ class Loadlitwa implements FixtureInterface
     public function load(ObjectManager $manager)
     {
 
-        $filename = __DIR__ . '/../../../../data/litwa.yml';
+        $filename = __DIR__ . '/../../../../data/italy.yml';
         $yml = Yaml::parse(file_get_contents($filename));
         foreach ($yml as $item) {
-            $litwa = new litwa();
-            $litwa->setName($item['name']);
-            $litwa->setpopulation($item['population']);
-            $manager->persist($litwa);
+            $italy = new Italy();
+            $italy->setName($item['name']);
+            $italy->setDepth($item['depth']);
+            $manager->persist($italy);
         }
 
         $manager->flush();
