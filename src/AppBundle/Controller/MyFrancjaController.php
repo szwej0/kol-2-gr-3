d716aa06a3ee8c0326c308a8e7e892d1335a9e1f
Sebastian Cytawa <spike123on@gmail.com>
2015-04-10 10:07:52 +0200

    Rivers: kontroler i widok

---
+++ src/AppBundle/Controller/MyFrancjaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MyFrancjaController extends Controller
+{
+
+    /**
+     * @Route("/francja.html", name="francja")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AppBundle:francja')->findAll();
+
+        return $this->render(
+            'default/francja.html.twig',
+            array(
+                'entities' => $entities,
+            )
+        );
+    }
+
+}
--- src/AppBundle/DataFixtures/ORM/LoadFrancja.php
+++ src/AppBundle/DataFixtures/ORM/LoadFrancja.php
@@ -7,9 +7,9 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 use Symfony\Component\Yaml\Yaml;
 
-use AppBundle\Entity\litwa;
+use AppBundle\Entity\Francja;
 
-class Loadlitwa implements FixtureInterface
+class LoadHungary implements FixtureInterface
 {
     /**
      * {@inheritDoc}
@@ -17,13 +17,13 @@ class Loadlitwa implements FixtureInterface
     public function load(ObjectManager $manager)
     {
 
-        $filename = __DIR__ . '/../../../../data/litwa.yml';
+        $filename = __DIR__ . '/../../../../data/francja.yml';
         $yml = Yaml::parse(file_get_contents($filename));
         foreach ($yml as $item) {
-            $litwa = new litwa();
-            $litwa->setName($item['name']);
-            $litwa->setpopulation($item['population']);
-            $manager->persist($litwa);
+            $francja = new Francja();
+            $francja->setName($item['name']);
+            $francja->setDepth($item['depth']);
+            $manager->persist($francja);
         }
 
         $manager->flush();
